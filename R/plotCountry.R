0bc7daa08f0fa0552411f0c6ac1352636fa4b032
Martin Zestermann <martin.zestermann@gmail.com>
2019-12-11 14:45:05 +0100

    bugfixing with .data

--- R/plotCountry.R
+++ R/plotCountry.R
@@ -1,5 +1,4 @@
 #' plotCountry
-#'
 #' plot the life expectancy vs GDP for a given country
 #'
 #' @param name country of gapminder
@@ -21,8 +20,7 @@ plotCountry <- function(name) {
 
   gapminder::gapminder %>% ggplot2::ggplot(ggplot2::aes(x = base::log10(.data$gdpPercap), y = .data$lifeExp, size = .data$pop))+
     ggplot2::geom_point(alpha = .25)+
-    ggplot2::geom_point(data = .data$gap_country, ggplot2::aes(color = .data$year))
+    ggplot2::geom_point(data = gap_country, ggplot2::aes(color = .data$year))
 
 }
-
 if(getRversion() >= "2.15.1")  utils::globalVariables(c(".data", "%>%"))
